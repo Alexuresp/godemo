c77efbf06cd40002c81c9bbfbbbff5a4ad70cd73
Your Name <you@example.com>
2026-07-21 11:04:53 +0300

    fix: manually bind config repository in Laravel bootstrap

--- laravel-app/bootstrap/app.php
+++ laravel-app/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Config\Repository as ConfigRepository;
 use Illuminate\Filesystem\FilesystemServiceProvider;
 use Illuminate\Translation\TranslationServiceProvider;
 use Illuminate\View\ViewServiceProvider;
@@ -22,7 +23,12 @@ $app = Application::configure(basePath: dirname(__DIR__))
     })
     ->create();
 
-$app->configure('app');
+$app->singleton('config', function ($app) {
+    $configPath = $app->basePath('config/app.php');
+    $config = is_file($configPath) ? require $configPath : [];
+    return new ConfigRepository($config);
+});
+
 $app->register(FilesystemServiceProvider::class);
 $app->register(ViewServiceProvider::class);
 $app->register(TranslationServiceProvider::class);
